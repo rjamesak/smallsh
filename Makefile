ef3e5d5cb318624061473293ef4dcf390cf1355b
Russell James <jamesru@oregonstate.edu>
2021-01-30 13:58:22 -0800

    Parse user input.

---
+++ Makefile
@@ -0,0 +1,39 @@
+## Russell James Makefile
+#CS 261, Winter 2019
+
+## variables for filenames
+prog1 = smallsh
+srcFile1 = smallsh.c
+#srcFile2 = Arena.cpp
+
+## variables for object names
+obj1 = $(srcFile1:.c=.o)
+#obj2 = $(srcFile2:.c=.o)
+
+## variables for header file names
+#header2 = $(srcFile2:.c=.hpp)
+
+## variables for compiler and flags
+CXX = gcc
+CXFlags = -c -std=gnu99 -g -Wall
+
+## valgrind flags
+VFlags = --leak-check=full -v
+
+#all: $(projName)
+
+$(prog1): $(obj1) 
+	$(CXX) -std=gnu99 -g $(obj1) -o $(prog1)
+
+$(obj1): $(srcFile1)
+	$(CXX) $(CXFlags) $(srcFile1)
+#
+#$(obj2): $(srcFile2) $(header2)
+#	$(CXX) $(CXFlags) $(srcFile2)
+#
+
+clean:
+	rm *.o $(prog1)
+
+valgrind:
+	valgrind $(VFlags) ./$(prog1)
---
+++ smallsh.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+struct userInput {
+	char* command;
+	char** arguments;
+};
+
+struct userInput* parseInput(char* inputLine, int inputLength);
+void freeInputStruct(struct userInput* inputStruct);
+
+int main(int argc, const char* argv[]) {
+	// create a command line
+	char* input = NULL;
+	char* exit = "exit";
+	size_t inputLength; 
+	ssize_t nread;
+	struct userInput* userInput = NULL;
+	do {
+		if (userInput) { 
+			freeInputStruct(userInput); 
+		};
+		printf(": ");
+		nread = getline(&input, &inputLength, stdin);
+		userInput = parseInput(input, (int) nread);
+	} while (strncmp(userInput->command, exit, 4) != 0);
+
+	// clean input struct
+	//free(input);
+	free(input);
+	freeInputStruct(userInput);
+
+}
+
+// receives the input line and parses the command
+// and arguments
+struct userInput* parseInput(char* inputLine, int inputLength)
+{
+	// init the struct
+	struct userInput* parsedInput = malloc(sizeof(struct userInput));
+	// use strtok to parse the input line
+	char* savePtr;
+	char* token = strtok_r(inputLine, " ", &savePtr);
+	// first input is the command
+	// NOTE strdup allocs mem, remember to free
+	parsedInput->command = strdup(token);
+
+	// loop to store the remaining arguments
+	parsedInput->arguments = calloc(inputLength, sizeof(char*)); //create array of char ptrs
+	int i = 0;
+	while ((token = strtok_r(NULL, " ", &savePtr)) != NULL) {
+		// store each arg
+		parsedInput->arguments[i] = strdup(token);
+		i++;
+	}
+	// terminate the arg array
+	//parsedInput->arguments[inputLength - 1] = '\0';
+
+	return parsedInput;
+}
+
+void freeInputStruct(struct userInput* inputStruct)
+{
+	// free the command
+	free(inputStruct->command);
+	// loop the arg array and free each
+	int i = 0;
+	while (inputStruct->arguments[i] != '\0') {
+		free(inputStruct->arguments[i]);
+		i++;
+	}
+	free(inputStruct->arguments);
+	free(inputStruct);
+}
